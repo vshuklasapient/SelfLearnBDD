1d067c4f7e4421dc8eb05d6cf9de9c2876c16ae8
Gauri <Gauri@LAPTOP-0B70KVE1>
2019-12-20 00:21:50 +0530

    # WARNING: head commit changed in the meantime
    Adding chrome driver

--- src/test/java/TestSelf/Learning/RunCukes.java
+++ src/test/java/TestSelf/Learning/RunCukes.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/TestSelf/Learning", format = {"pretty",
-        "html:target/cucumber-reports", "json:target/cucumber-reports/cucumber.json"}, tags = {"@complete"})
+@CucumberOptions(features = "src/test/java/TestSelf/Learning", plugin = {"pretty",
+        "html:target", "json:target/cucumber.json"}, tags = {"@complete"})
 public class RunCukes {
-}
+} 
